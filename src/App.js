38fbbd5be8a731fd4d1b0248b6096491a6e06eb0
pena56 <ogbopinamoses@gmail.com>
2020-12-17 14:32:02 +0100

    added debug.log and eslintcache to gitignore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 
+import Header from './components/Header';
+import Todo from './components/Todo';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="">
+      <Header />
+      <Todo />
+    </div>
+  );
 }
 
 export default App;
